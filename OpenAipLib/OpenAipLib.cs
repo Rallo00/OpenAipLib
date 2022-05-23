1920c799272eb3c0ea2f4fd96b5226cf083f6049
dfergusson8 <dfergusson8@gmail.com>
2022-05-23 11:06:15 +0200

    First Commit
    - Added functional OpenAIP Library.

---
+++ OpenAipLib/OpenAipLib.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+//URL DI ESEMPIO: 
+//https://api.core.openaip.net/api/airports?fields=_id%2Cname%2CicaoCode%2CiataCode%2Ccountry%2Cgeometry%2Celevation%2Cfrequencies%2Crunways%2Ccontact%2Cremarks&sortDesc=true&approved=true&searchOptLwc=true&search=lipr&apiKey=1eba052dd328ac3b9894d0c3d62678a6
+//DOC:
+//https://docs.openaip.net/#/Airports/get_airports
+
+public static class OpenAipLib
+{
+    public static async Task<Airport> GetAirportInfo(string icao, string API_TOKEN)
+    {
+        string URI = $"https://api.core.openaip.net/api/airports?page=1&limit=1&fields=_id%2Cname%2CicaoCode%2CiataCode%2Ccountry%2Cgeometry%2Celevation%2Cfrequencies%2Crunways%2Ccontact%2Cremarks&sortDesc=true&approved=true&searchOptLwc=true&search={icao.ToUpper()}&apiKey={API_TOKEN}";
+        string response = await Http_GetRequest(URI);
+        //Converting single result (for sure it's single because prompted in query string as ICAO code is unique)
+        OpenAipResult result = Newtonsoft.Json.JsonConvert.DeserializeObject<OpenAipResult>(response);
+        JsonAirport airport = result.AirportData[0];
+
+        Airport convertedAirport = new Airport();
+        convertedAirport.Name = airport.Name;
+        convertedAirport.Icao = airport.Icao;
+        convertedAirport.Iata = airport.Iata;
+        convertedAirport.Country = airport.Country;
+        convertedAirport.Elevation = airport.Elevation.Value;
+        convertedAirport.Frequencies = airport.Frequencies;
+        //Fix Runway
+        List<Runway> runways = new List<Runway>();
+        for(int i = 0; i < airport.Runways.Count; i+=2)
+        {
+            string rwy = $"{airport.Runways[i].RunwayNumber}\\{airport.Runways[i + 1].RunwayNumber}";
+            string trh = $"{airport.Runways[i].TrueHeading}\\{airport.Runways[i+1].TrueHeading}";
+            runways.Add(new Runway(rwy, trh, airport.Runways[i].Dimensions.RwyLength.Value, airport.Runways[i].Dimensions.RwyWidth.Value));
+        }
+        convertedAirport.Runways = runways;
+        //Fix Frequencies
+        foreach(Frequency f in airport.Frequencies)
+            switch (f.Type)
+            {
+                case "0": f.Type = "Approach"; break;
+                case "1": f.Type = "APRON"; break;
+                case "2": f.Type = "Arrival"; break;
+                case "3": f.Type = "Center"; break;
+                case "4": f.Type = "CTAF"; break;
+                case "5": f.Type = "Delivery"; break;
+                case "6": f.Type = "Departure"; break;
+                case "7": f.Type = "FIS"; break;
+                case "8": f.Type = "Gliding"; break;
+                case "9": f.Type = "Ground"; break;
+                case "10": f.Type = "Info"; break;
+                case "11": f.Type = "Multicom"; break;
+                case "12": f.Type = "Unicom"; break;
+                case "13": f.Type = "Radar"; break;
+                case "14": f.Type = "Tower"; break;
+                case "15": f.Type = "ATIS"; break;
+                case "16": f.Type = "Radio"; break;
+                case "17": f.Type = "Other"; break;
+                case "18": f.Type = "AIRMET"; break;
+                case "19": f.Type = "AWOS"; break;
+                case "20": f.Type = "Lights"; break;
+                case "21": f.Type = "VOLMET"; break;
+                default: f.Type = "Not available"; break;
+            }
+        //Fix Coordinates
+        convertedAirport.Latitude = airport.Geometry.Coordinates[0];
+        convertedAirport.Longitude = airport.Geometry.Coordinates[1];
+
+        return convertedAirport;
+    }
+    private static async Task<string> Http_GetRequest(string URI)
+    {
+        string result;
+        System.Net.HttpWebRequest request = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(URI);
+        using (System.Net.HttpWebResponse response = (System.Net.HttpWebResponse)await request.GetResponseAsync())
+        using (System.IO.Stream stream = response.GetResponseStream())
+        using (System.IO.StreamReader reader = new System.IO.StreamReader(stream))
+            result = await reader.ReadToEndAsync();
+        return result;
+    }
+    public class Airport
+    {
+        public string Name;
+        public string Icao;
+        public string Iata;
+        public string Country;
+        public double Elevation;
+        public string Latitude;
+        public string Longitude;
+        public List<Frequency> Frequencies;
+        public List<Runway> Runways;
+        public Airport() { }
+    }
+    public class Elevation
+    {
+        [Newtonsoft.Json.JsonProperty("value")]     //OpenAIP gives back always meters
+        public double Value;
+        public Elevation(double v)
+        {
+            Value = v;
+        }
+    }
+    public class Frequency
+    {
+        //"frequencies":[{"value":"121.600","unit":2,"type":9,"name":"RIMINI GND","primary":false,"publicUse":true,"_id":"62614c185e9ded571044b44e"},
+        [Newtonsoft.Json.JsonProperty("value")]
+        public string Value;
+        [Newtonsoft.Json.JsonProperty("name")]
+        public string Name;
+        [Newtonsoft.Json.JsonProperty("type")]
+        public string Type;
+        public Frequency(string v, string n, string t)
+        {
+            Value = v; Name = n;
+        }
+    }
+    public class Runway
+    {
+        public string Number;
+        public string TrueHeading;
+        public string Length;           //OpenAIP gives back always METERS
+        public string Width;            //
+        public Runway(string n, string th, string l, string w)
+        {
+            Number = (n != null) ? n : "Not available"; 
+            TrueHeading = (th != null) ? th : "Not available"; 
+            Length = (l != null) ? l : "Not available"; 
+            Width = (w != null) ? w : "Not available";
+        }
+    }
+
+    //PRIVATE CLASSES FOR INTERNAL USE AND AIRPORT STRUCTURE CONVERSION
+    protected internal class OpenAipResult
+    {
+        [Newtonsoft.Json.JsonProperty("items")]
+        protected internal List<JsonAirport> AirportData;
+
+        [JsonConstructor]
+        private OpenAipResult(List<JsonAirport> result)
+        {
+            AirportData = result;
+        }
+    }
+    protected internal class JsonAirport
+    {
+        [Newtonsoft.Json.JsonProperty("name")]
+        protected internal string Name;
+        [Newtonsoft.Json.JsonProperty("icaoCode")]
+        protected internal string Icao;
+        [Newtonsoft.Json.JsonProperty("iataCode")]
+        protected internal string Iata;
+        [Newtonsoft.Json.JsonProperty("country")]
+        protected internal string Country;
+        [Newtonsoft.Json.JsonProperty("elevation")]
+        protected internal Elevation Elevation;
+        [Newtonsoft.Json.JsonProperty("frequencies")]
+        protected internal List<Frequency> Frequencies;
+        [Newtonsoft.Json.JsonProperty("runways")]
+        protected internal List<JsonRunway> Runways;
+        [Newtonsoft.Json.JsonProperty("geometry")]
+        protected internal JsonGeometry Geometry;
+
+        [JsonConstructor]
+        private JsonAirport(string n, string icao, string iata, string c, /*string coord,*/ Elevation elev, List<Frequency> freq)
+        {
+            Name = n; Icao = icao; Iata = iata; Country = c; /*Coordinates = coord;*/ Elevation = elev; Frequencies = freq;
+        }
+    }
+    protected internal class JsonRunway
+    {
+        [Newtonsoft.Json.JsonProperty("designator")]
+        protected internal string RunwayNumber;
+        [Newtonsoft.Json.JsonProperty("trueHeading")]
+        protected internal string TrueHeading;
+        [Newtonsoft.Json.JsonProperty("dimension")]
+        protected internal JsonRwyDimensions Dimensions;
+
+        [JsonConstructor]
+        private JsonRunway(string r, string th, JsonRwyDimensions rd) { RunwayNumber = r; TrueHeading = th; Dimensions = rd; }
+    }
+    protected internal class JsonRwyDimensions
+    {
+        [Newtonsoft.Json.JsonProperty("length")]
+        protected internal JsonRwyLength RwyLength;
+        [Newtonsoft.Json.JsonProperty("width")]
+        protected internal JsonRwyWidth RwyWidth;
+    }
+    protected internal class JsonRwyLength
+    {
+        [Newtonsoft.Json.JsonProperty("value")]
+        protected internal string Value;
+        [Newtonsoft.Json.JsonProperty("unit")]
+        protected internal string Unit;
+    }
+    protected internal class JsonRwyWidth
+    {
+        [Newtonsoft.Json.JsonProperty("value")]
+        protected internal string Value;
+        [Newtonsoft.Json.JsonProperty("unit")]
+        protected internal string Unit;
+    }
+    protected internal class JsonGeometry
+    {
+        [Newtonsoft.Json.JsonProperty("type")]
+        protected internal string Type;
+        [Newtonsoft.Json.JsonProperty("coordinates")]
+        protected internal List<string> Coordinates;
+    }
+}
---
+++ OpenAipLibTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenAipLibTest
+{
+    internal class Program
+    {
+        static string API_TOKEN = "1eba052dd328ac3b9894d0c3d62678a6";
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Cerca un aeroporto (ICAO): ");
+            string airportIcao = Console.ReadLine();
+            GetAirportData(airportIcao.ToUpper());
+            Console.ReadKey();
+        }
+        static async void GetAirportData(string icao)
+        {
+            OpenAipLib.Airport airport = await OpenAipLib.GetAirportInfo(icao, API_TOKEN);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(">> AIRPORT RESULT");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"City: {airport.Name}");
+            Console.WriteLine($"Country: {airport.Country}");
+            Console.WriteLine($"ICAO: {airport.Icao}");
+            Console.WriteLine($"IATA: {airport.Iata}");
+            Console.WriteLine($"Coordinates: {airport.Latitude}°N, {airport.Longitude}°E");
+            Console.WriteLine($"Elevation: {airport.Elevation} m");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Frequencies:");
+            Console.ForegroundColor = ConsoleColor.White;
+            foreach (OpenAipLib.Frequency f in airport.Frequencies)
+                Console.WriteLine($"{f.Name}\t{f.Type}\t{f.Value}");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Runway:");
+            Console.ForegroundColor = ConsoleColor.White;
+            foreach (OpenAipLib.Runway r in airport.Runways)
+                Console.WriteLine($"{r.Number}\t{r.TrueHeading}\t{r.Length} m\t{r.Width} m");
+        }
+    }
+}
